89126e010116391a8aefcda5c40b58de2247ddba
phongpv <vuphongshyn1998@gmail.com>
2022-07-24 15:57:08 +0700

    connect db mysql

--- src/server.js
+++ src/server.js
@@ -1,17 +1,20 @@
-import express from "express"
-import bodyParser from "body-parser"
-import viewEngine from "./config/viewEngine"
-import initWebRoutes from "./route/web"
-require('dotenv').config()
+import express from "express";
+import bodyParser from "body-parser";
+import viewEngine from "./config/viewEngine";
+import initWebRoutes from "./route/web";
+require("dotenv").config();
+import connectDB from "./config/connectDB";
 
-let app = express()
+let app = express();
 
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({ extended: true }))
-viewEngine(app)
-initWebRoutes(app)
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+viewEngine(app);
+initWebRoutes(app);
+
+connectDB();
 
 let port = process.env.PORT || 6969;
 app.listen(port, () => {
-    console.log("Backend Nodejs is running port:" + port)
-})
+  console.log("Backend Nodejs is running port:" + port);
+});
